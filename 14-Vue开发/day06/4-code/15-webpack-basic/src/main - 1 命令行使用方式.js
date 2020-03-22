49a9c533810d49034c6eea5c5e9a8c01b31f31c2
secley <secley@qq.com>
2020-03-22 20:56:53 +0800

    0322-04

--- 14-Vue开发/day06/4-code/15-webpack-basic/src/main - 1 命令行使用方式.js
+++ 14-Vue开发/day06/4-code/15-webpack-basic/src/main - 1 命令行使用方式.js
@@ -1,8 +1,8 @@
 // webpack 的使用步骤：
 // 1 安装： npm i -D webpack webpack-cli
 // 2 webpack 使用的两种方式：
-//  2.1 命令行的使用方式（知道）
-//  2.2 配置文件
+//  2.1 命令行的使用方式（知道即可）
+//  2.2 配置文件 (推荐)
 
 // webpack的四个核心概念：
 // 1 入口 entry
@@ -11,8 +11,8 @@
 // 4 plugins 插件
 
 // 演示命令行的使用方式：
-// webpack 入口文件 出口文件路径
-// 最基本的打包: webpack ./src/main.js
+// 打包命令: webpack 入口文件 出口文件路径(打包后内容输出的目录)
+// 最基本的打包命令: webpack ./src/main.js (默认出口文件输出在dist目录下)
 
 // 注意:使用 webpack 的时候应该提供mode, 可以是: production 或者 development
 // production 表示: 生产模式    -- 生产环境(也就是给用户使用的)
@@ -21,8 +21,8 @@
 // 指定模式: .\node_modules\.bin\webpack ./src/main.js --mode development
 // 指定为生产模式: .\node_modules\.bin\webpack ./src/main.js --mode production
 
-// 演示指定出口文件路径:
-//  .\node_modules\.bin\webpack ./src/main.js -o ./dist/a.js --mode production
+// 演示指定出口文件路径的打包 (不用默认出口文件路径) :
+//  .\node_modules\.bin\webpack ./src/main.js -o ./dist/a.js --mode production  // 需要指定输出的文件名, 将来将打包输出的内容输出到此文件中
 
 const fn = () => {
   console.log('main.js 中的fn 执行了')
