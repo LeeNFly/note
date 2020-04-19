69c243d091acee5d9a088982136ff908b41c54b0
secley <secley@qq.com>
2020-04-19 11:12:17 +0800

    0419-02

--- 18-TypeScript/01Typescript介绍 、Typescript安装、Typescript开发工具(15分51秒）/tsdemo01/index.ts
+++ 18-TypeScript/01Typescript介绍 、Typescript安装、Typescript开发工具(15分51秒）/tsdemo01/index.ts
@@ -2,7 +2,7 @@
  * @Author: Ling Hui Shi
  * @Date: 2020-04-18 22:44:31
  * @LastEditors: Ling Hui Shi
- * @LastEditTime: 2020-04-18 23:22:45
+ * @LastEditTime: 2020-04-19 11:06:53
  * @Description: 
  */
 console.log('你好ts')
@@ -17,10 +17,11 @@ var str:string="你好ts";
 /*
     什么是TypeScript?
     TypeScript是一种由微软开发的开源、跨平台的编程语言。它是JavaScript的超集，最终会被编译为JavaScript代码。
-    TypeScript 简称ts, TypeScript 和 JavaScript是两种不同的语言, 各自语法也不同. 
+    TypeScript 简称ts, TypeScript语言 和 JavaScript语言是两种不同的语言, 各自语法也不同. 但是ts语法类似于js语法
     TypeScript 文件后缀名为.ts, .ts文件中直接写TypeScript代码即可, 浏览器不能直接识别TypeScript, TypeScript需要被编译成JavaScript后, 才能被浏览器作为JS代码解析和执行
     .ts文件 (ts代码) 最终会被编译成一个.js文件 (js代码), 最终在页面引入的是.ts文件编译后得到的.js文件, 即页面引入的还是.js文件, 浏览器解析和执行的还是js代码
-*/
+    vue, react, webpack 也可以集成, 支持TypeScript
+    */
 
 /*
 vscode配置自动编译
